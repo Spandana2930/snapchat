d6f79a033927b0b30fdb546e15f9362461cf79ac
Spandana Morampudi <smorampudi@innominds.com>
2021-11-09 13:04:33 +0530

    first initialization

---
+++ frontend/client/components/App/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1>UGF5H4GF</h1>
+    </>
+   
+  );
+}
+
+export default App;
